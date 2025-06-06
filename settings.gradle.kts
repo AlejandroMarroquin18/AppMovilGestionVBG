92a321eee0fbe964887278823a965ab9e832d909
FalaisePocket <victordanielval@gmail.com>
2025-06-05 22:31:45 -0500

    Gradle arreglado

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,20 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
     }
-
-    // ✅ Use this only if you need to include another file.
     versionCatalogs {
         create("libs") {
-            //from(files("gradle/libs.versions.toml"))
+
         }
     }
 }
