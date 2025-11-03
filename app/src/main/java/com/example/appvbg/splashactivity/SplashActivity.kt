2ef39168f7f45eb506f9cd007fc86ecdd2aeaa54
FalaisePocket <victordanielval@gmail.com>
2025-11-03 17:41:49 -0500

    Añadido Checksession

--- app/src/main/java/com/example/appvbg/splashactivity/SplashActivity.kt
+++ app/src/main/java/com/example/appvbg/splashactivity/SplashActivity.kt
@@ -5,9 +5,18 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import com.example.appvbg.APIConstant
 import com.example.appvbg.MainActivity
 import com.example.appvbg.R
+import com.example.appvbg.api.makeRequest
 import com.example.appvbg.loginactivity.LoginActivity
+import com.example.appvbg.api.PrefsHelper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
 
 class SplashActivity : AppCompatActivity() {
     private val SPLASH_DELAY: Long = 1000 // 1 segundo
@@ -15,30 +24,50 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        // Ocultar la action bar
         supportActionBar?.hide()
-
-        setContentView(R.layout.activity_splash)
-
-
-
 
         val sharedPref = getSharedPreferences("AppPrefs", MODE_PRIVATE)
         val token = sharedPref.getString("authToken", null)
 
         Handler(Looper.getMainLooper()).postDelayed({
-
             if (token != null) {
-                startActivity(Intent(this, MainActivity::class.java))
+
+                lifecycleScope.launch(Dispatchers.IO) {
+                    try {
+                        val checkSession = makeRequest(
+                            """${APIConstant.BACKEND_URL}api/auth/checkSession/""",
+                            "GET",
+                            PrefsHelper.getDRFToken(applicationContext).toString()
+                        )
+
+                        if (checkSession != "error") {
+                            withContext(Dispatchers.Main) {
+                                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                                finish()
+                            }
+                        } else {
+                            withContext(Dispatchers.Main) {
+                                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                                finish()
+                            }
+                        }
+
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        withContext(Dispatchers.Main) {
+                            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                            finish()
+                        }
+                    }
+                }
+
             } else {
-                startActivity(Intent(this, LoginActivity::class.java))
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                finish()
             }
-            finish()
-
         }, SPLASH_DELAY)
-
-
     }
 }
+
 
 
